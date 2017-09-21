8dd19238c61c5c1c870c356decae9921b583ef7c
Anass Oumiri <aoumiri@octo.com>
2017-09-21 22:46:36 +0200

    Step 1 : App name via props

--- src/App/App.spec.js
+++ src/App/App.spec.js
@@ -7,8 +7,15 @@ import App from './App';
 chai.use(chaiEnzyme());
 
 describe('App component', () => {
-  it('should display Hello world', () => {
+  it('should render default hello message', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper).to.have.text('Hello world');
+
+    expect(wrapper).to.contain.text('Hello OCTO !');
+  });
+
+  it('Should render hello message with the right props', () => {
+    const wrapper = shallow(<App name={'myName'} />);
+
+    expect(wrapper).to.contain.text('Hello myName !');
   });
 });
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App/App';
 
-render(<App />, document.getElementById('root'));
+const APP_NAME = 'Microblog React';
+
+render(<App name={APP_NAME} />, document.getElementById('root'));
 
 if (module.hot) {
   module.hot.accept('./App/App.js', () => {
     const UpdatedAppComponent = require('./App/App.js').default; // eslint-disable-line global-require
-    render(<UpdatedAppComponent />, document.getElementById('root'));
+    render(<UpdatedAppComponent name={APP_NAME} />, document.getElementById('root'));
   });
 }
